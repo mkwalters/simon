2faab4ac85b021ebc8cc7688e9dcd3a8601cde06
Mitchell Walters <mitchell.walters@giant.cos.edu>
2015-12-01 23:46:51 -0800

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,26 @@
+display_width = 710
+display_height = 600
+button_length = 200
+still_playing = True
+green_button_x = 100
+green_button_y = 75
+red_button_x = 400
+red_button_y = 75
+yellow_button_x = 100
+yellow_button_y = 350
+blue_button_x = 400
+blue_button_y = 350
+
+background_color = (200,200,200)
+
+red_color = (128,0,0)
+green_color = (0,128,0)
+yellow_color = (128,128,0)
+blue_color = (0,0,128)
+
+red_lit_color = (255,0,0)
+green_lit_color = (0,255,0)
+yellow_lit_color = (255,255,0)
+blue_lit_color = (0,0,255)
+
+time_between_flashes = 1000
