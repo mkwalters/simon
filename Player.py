1bf23cd9b4508e598d53e6d693a34aa656ff9e3f
Mitchell Walters <mitchell.walters@giant.cos.edu>
2015-12-01 23:49:01 -0800

    Create Player.py

---
+++ Player.py
@@ -0,0 +1,9 @@
+
+class Player():
+
+	def click_square(self,x,y):
+		solutions.build_solution_set()
+		solutions.build_solution_times()
+		
+		#these are just here for debugging purposes. 
+		#you can go ahead and ignore this class for now
