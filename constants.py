c39a3ca3da3ed45156feac53251bd3962dc421d7
Mitchell Walters <mitchell.walters@giant.cos.edu>
2015-12-03 14:32:42 -0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -11,12 +11,28 @@ yellow_button_y = 350
 blue_button_x = 400
 blue_button_y = 350
 
+score_display_x = 10
+score_display_y = 10
+score_display_color = (0,0,0) #black
+score_display_size = 100
+
+loss_display_x = 200
+loss_display_y = 250
+loss_display_color = (0,0,0) #black
+loss_message = "You lose!"
+
 background_color = (200,200,200)
 
 red_color = (128,0,0)
 green_color = (0,128,0)
 yellow_color = (128,128,0)
 blue_color = (0,0,128)
+
+left_x = green_button_x
+top_y = green_button_y
+
+right_x = blue_button_x
+bottom_y = blue_button_y
 
 red_lit_color = (255,0,0)
 green_lit_color = (0,255,0)
